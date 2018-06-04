a98c993877f2ccd7ee61c6b05c1db19e92d6bf45
YuChengKai <597813039@qq.com>
2018-06-04 22:46:42 +0800

    最长递增子序列

---
+++ algorithm/DP/LIS.js
@@ -0,0 +1,22 @@
+function lis(n) {
+  if (n.length === 0) return 0
+  // 创建一个和参数相同大小的数组，并填充值为 1
+  let array = new Array(n.length).fill(1)
+  // 从索引 1 开始遍历，因为数组已经所有都填充为 1 了
+  for (let i = 1; i < n.length; i++) {
+    // 从索引 0 遍历到 i
+    // 判断索引 i 上的值是否大于之前的值
+    for (let j = 0; j < i; j++) {
+      if (n[i] > n[j]) {
+        array[i] = Math.max(array[i], 1 + array[j])
+      }
+    }
+  }
+  let res = 1
+  for (let i = 0; i < array.length; i++) {
+    res = Math.max(res, array[i])
+  }
+  return res
+}
+
+console.log(lis([0, 3, 4, 17, 2, 8, 6, 10]))
